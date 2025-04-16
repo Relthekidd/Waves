28d60e94ba0af9ad0e9a57f43bf811989faca990
Relthekidd <relthekidd@gmail.com>
2025-04-16 16:05:46 -0400

    Create Home.tsx

---
+++ app/(tabs)/Home.tsx
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Tab</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0f0f0f',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 28,
+    color: '#ffffff',
+    fontWeight: '700',
+  },
+});
